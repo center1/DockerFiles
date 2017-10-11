e13fe89070b7c4c8e2f0a25a470f7bd98065265a
joethorpe6 <joethorpe6@openmailbox.org>
2017-10-11 19:57:41 +0100

    new instantdomainadmin container

---
+++ InstantDomainAdmin/launcher.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+tmux new-session -d -s instantdomainadmin ;
+tmux send-keys -t instantdomainadmin "crackmapexec | less "  C-m
+tmux new-window \;
+tmux split-window -h ;
+tmux select-pane -L
+tmux send-keys -t instantdomainadmin "cd /opt/Empire && ./empire --rest --username username --password password" C-m
+tmux select-pane -R
+tmux send-keys -t instantdomainadmin "sleep 5 && cd /opt/DeathStar && python3 ./DeathStar.py -u username -p password "  C-m
+tmux attach-session -d -t instantdomainadmin
