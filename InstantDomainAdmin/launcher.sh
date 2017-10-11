6efab829ae990ac0891df39f1997a8b13e2bf161
joethorpe6 <joethorpe6@openmailbox.org>
2017-10-11 22:42:07 +0100

    change to sleep 10

--- InstantDomainAdmin/launcher.sh
+++ InstantDomainAdmin/launcher.sh
@@ -6,5 +6,5 @@ tmux split-window -h ;
 tmux select-pane -L
 tmux send-keys -t instantdomainadmin "cd /opt/Empire && ./empire --rest --username username --password password" C-m
 tmux select-pane -R
-tmux send-keys -t instantdomainadmin "sleep 5 && cd /opt/DeathStar && python3 ./DeathStar.py -u username -p password "  C-m
+tmux send-keys -t instantdomainadmin "sleep 10 && cd /opt/DeathStar && python3 ./DeathStar.py -u username -p password "  C-m
 tmux attach-session -d -t instantdomainadmin
