8663a36a55a69a674fada505eaf1d777e9cfdb99
joethorpe6 <joethorpe6@openmailbox.org>
2017-10-12 15:06:27 +0100

    keeping things consistant

--- InstantDomainAdmin/launcher.sh
+++ InstantDomainAdmin/launcher.sh
@@ -9,7 +9,7 @@ tmux send-keys -t instantdomainadmin "crackmapexec | less "  C-m
 tmux new-window \;
 tmux split-window -h ;
 tmux select-pane -L
-tmux send-keys -t instantdomainadmin "cd /opt/Empire && ./empire --rest --username username --password password" C-m
+tmux send-keys -t instantdomainadmin "cd /opt/Empire && ./empire --rest --username empireadmin --password Password123!" C-m
 tmux select-pane -R
-tmux send-keys -t instantdomainadmin "sleep 10 && cd /opt/DeathStar && python3 ./DeathStar.py -u username -p password -lip $HOSTIP"  C-m
+tmux send-keys -t instantdomainadmin "sleep 10 && cd /opt/DeathStar && python3 ./DeathStar.py -u empireadmin -p Password123! -lip $HOSTIP"  C-m
 tmux attach-session -d -t instantdomainadmin
